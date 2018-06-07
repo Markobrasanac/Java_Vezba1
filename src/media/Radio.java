6148dffe915503d4817ba08935adb41c4ba7bf7c
Marko <markobra123@gmail.com>
2018-06-07 19:01:28 +0200

    Java vezba 1. -drugi deo

---
+++ src/media/Radio.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package media;
+
+/**
+ *
+ * @author qa-popodne
+ */
+public class Radio {
+
+    private double fmFrequency;
+    private int mFrequency;
+    private char band;
+
+    public double getFmFrequency() {
+        return fmFrequency;
+    }
+
+    public void setFmFrequency(double fmFrequency) {
+        this.fmFrequency = fmFrequency;
+    }
+
+    public int getmFrequency() {
+        return mFrequency;
+    }
+
+    public void setmFrequency(int mFrequency) {
+        this.mFrequency = mFrequency;
+    }
+
+    public char getBand() {
+        return band;
+    }
+
+    public void setBand(char band) {
+        this.band = band;
+    }
+
+}
