00b2f2eff2e759c6b675b56bbb6a2cf6aee49906
Marko <markobra123@gmail.com>
2018-06-07 18:48:48 +0200

    JAva Vezba1

---
+++ src/media/Television.java
@@ -0,0 +1,37 @@
+
+package media;
+
+
+public class Television {
+    
+    private int volume = 0;
+    private int currentProgram = 1;
+    private boolean turnOn = true;
+    
+    
+    public int getVolume() {
+        return volume;
+    }
+    
+    public void  setVolume(int volume) {
+        this.volume = volume;
+    }
+    
+    
+    public int getCurrentProgram() {
+        return currentProgram;
+    }
+    
+    public void  setCurrentProgram(int currentProgram) {
+        this.currentProgram = currentProgram;
+    }
+    
+    
+    public boolean getTurnOn() {
+        return turnOn;
+    }
+    
+    public void  setTurnOn(boolean turnOn) {
+        this.turnOn = turnOn;
+    }
+}
